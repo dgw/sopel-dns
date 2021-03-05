39ae11acde0a9c60342e6adcab8e67d6f10a1cd8
dgw <dgw@technobabbl.es>
2021-03-05 01:08:28 -0600

    TWeak rate-limiting & add output prefix to plugin's messages
    Kind of had to do these in one commit, or spend ages manually editing
    diff hunks. I chose the easy way.
    
    Also improves an error message or two.

--- sopel_dns/__init__.py
+++ sopel_dns/__init__.py
@@ -29,6 +29,7 @@ IMPLEMENTED_RDTYPES = ONELINE_RDTYPES + MULTILINE_RDTYPES
 @module.commands('dns')
 @module.example('.dns domain.tld AAAA', user_help=True)
 @module.example('.dns domain.tld', user_help=True)
+@module.output_prefix('[dns] ')
 @module.rate(user=300)
 def get_dnsinfo(bot, trigger):
     """Look up DNS information for a domain name."""
@@ -48,36 +49,36 @@ def get_dnsinfo(bot, trigger):
         else:
             answers = dns.resolver.resolve(domain, rdtype)
 
-    except dns.exception.Timeout:
-        bot.reply("DNS lookup timed out for {}.".format(domain))
-        return module.NOLIMIT
-    except dns.resolver.NXDOMAIN:
-        bot.reply("DNS lookup returned NXDOMAIN for {}.".format(domain))
-        return module.NOLIMIT
-    except dns.resolver.NoNameservers:
-        bot.reply("DNS lookup attempted, but no nameservers were available.")
-        return module.NOLIMIT
-    except dns.resolver.NoAnswer:
-        bot.reply("No {} records exist for {}.".format(rdtype, domain))
-        return module.NOLIMIT
     except dns.exception.SyntaxError:
         if rdtype == 'PTR':
             bot.reply("PTR record lookup is only supported for IP addresses.")
         else:
             bot.reply("That domain name doesn't seem to be valid.")
         return module.NOLIMIT
+    except dns.exception.Timeout:
+        bot.say("DNS lookup timed out for {}.".format(domain))
+        return module.NOLIMIT
+    except dns.resolver.NoNameservers:
+        bot.say("DNS lookup attempted, but no nameservers were available.")
+        return module.NOLIMIT
+    except dns.resolver.NXDOMAIN:
+        bot.say("DNS lookup returned NXDOMAIN for {}.".format(domain))
+        return  # do rate-limit, since query succeeded
+    except dns.resolver.NoAnswer:
+        bot.say("DNS lookup returned no {} records for {}.".format(rdtype, domain))
+        return  # do rate-limit, since query succeeded
 
     if len(answers) > 0:
         for rdata in answers:
             responses.append(rdata.to_text())
     else:
-        bot.reply("Did not find any {} records for {}.".format(rdtype, domain))
+        bot.say("Did not find any {} records for {}.".format(rdtype, domain))
         return
 
     if rdtype in ONELINE_RDTYPES:
-        bot.reply(', '.join([str(x) for x in responses]))
+        bot.say(', '.join([str(x) for x in responses]))
         return
 
     # Record types that should be handled one response per line
     for x in responses:
-        bot.reply(str(x))
+        bot.say(str(x))
